1010f2746a451560ec5408c6a486f93f6817f089
Basit-qc <basit.qc@gmail.com>
2021-01-19 11:47:06 +0500

    logging added for each skip reason

--- src/extension.ts
+++ src/extension.ts
@@ -39,20 +39,40 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log(`repoPath: ${repoPath}, branchName: ${branch}`);
 	vscode.workspace.onDidChangeTextDocument(changeEvent => {
 		if (!changeEvent.contentChanges.length) { return; }
+		const time = new Date().getTime();
 		// If you only care about changes to the active editor's text,
 		//  just check to see if changeEvent.document matches the active editor's document.
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) { return; }
+		if (editor.document !== changeEvent.document) {
+			console.log("Skipping InActive Editor's document");
+			return;
+		}
 		const filePath = changeEvent.document.fileName;
 		const text = changeEvent.document.getText();
-		if (!repoPath) { return; }
+		if (!repoPath) { 
+			console.log(`Skipping ${time} because of no repoPath`);
+			return; 
+		}
 		const relPath = filePath.split(`${repoPath}/`)[1];
 		const shadowPath = `${CODESYNC_ROOT}/${repoName}/${branch}/${relPath}`;
 		const shadowExists = fs.existsSync(shadowPath);
 		if (!shadowExists) { 
 			// TODO: Create shadow file?
+			console.log(`Skipping ${time} because shadow does not exist`);
 			return;
 		}
 		// Read shadow file 
 		const shadowText = fs.readFileSync(shadowPath, "utf8");
+		// If shadow text is same as current content, no need to compute diffs
+		if (shadowText === text) {
+			console.log(`Skipping ${time} because shadow is same as text`);
+			return;
+		}
+		if (!shadowText) {
+			console.log(`Skipping ${time} because shadow is empty`);
+			return;
+		}
 		// Update shadow file 
 		fs.writeFile(shadowPath, text, function (err) {
 			if (err) throw err;
@@ -63,7 +83,10 @@ export function activate(context: vscode.ExtensionContext) {
 		//  Create text representation of patches objects
 		const diffs = dmp.patch_toText(patches);
 		// Skip empty diffs
-		if (!diffs) { return; }
+		if (!diffs) { 
+			console.log(`Skipping ${time} because of empty diffs`);
+			return;
+		}
 		// Add new diff in the buffer
 		const newDiff = <IDiff>{};
 		newDiff.repo = repoName;
@@ -72,7 +95,7 @@ export function activate(context: vscode.ExtensionContext) {
 		newDiff.diff = diffs;
 		newDiff.created_at = dateFormat(new Date(), 'UTC:yyyy-mm-dd HH:MM:ss.l');
 		// Append new diff in the buffer
-		fs.writeFileSync(`${DIFFS_REPO}/${new Date().getTime()}.yml`, yaml.safeDump(newDiff));	
+		fs.writeFileSync(`${DIFFS_REPO}/${new Date().getTime()}.yml`, yaml.safeDump(newDiff));
 	});			
 	// context.subscriptions.push(disposable);
 }
