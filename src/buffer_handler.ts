3c518e2db4660ccb5e611a082bcf9b488a65b970
Basit-qc <basit.qc@gmail.com>
2021-06-02 17:24:58 +0500

    Docs added for handleBuffer, Fixed order of uploading diffs after authentication

--- src/buffer_handler.ts
+++ src/buffer_handler.ts
@@ -74,6 +74,51 @@ const connect = async() => {
 };
 
 export async function handleBuffer() {
+	/***
+	 * Each file in .diffs directory contains following data
+
+        repo_path: /Users/basit/projects/codesync/codesync
+        branch: plugins
+        file: codesync/init.py
+        diff: |
+         @@ -14070,8 +14070,10 @@
+             pass%0A
+         +#
+        created_at: '2021-01-01 11:49:36.121'
+
+	Steps:
+		- Get list of diffs (only .yml files)
+		- If there are diffs founds
+		- Check if server is up
+			- Do not continue if server is down
+		- Validate structure of JSON in diff file
+			- Skip invalid diff files
+		- Group diffs by repo path to send multiple in 1 iteration
+		- For each diff group
+			- Authenticate from server for repo token
+				- Do not continue if token is invalid
+		- For each diff in diffs-group
+			- Check if file_relative_path in diff file is syncable, 
+				- Skip the diff-file if it is for non-syncable file
+			- If diff is for changes for existing file
+				- Push changes to server
+				- Remove the diff file if data is successfully uploaded
+			- If diff is for new file
+				- Upload file to server & then on s3
+				- Updated config.yml with new file ID
+				- Remove the diff file if data is successfully uploaded
+			- If diff is for rename file
+				- rename file in shadow repo
+				- rename file in config.yml
+				- Push rename-diff to server
+				- Remove the diff file if data is successfully uploaded
+			- If diff is for directory rename
+				- Repeat file rename for every nested item
+			- If diff is for is_deleted
+				- Get the diff with shadow file
+				- Remove the sahdow file
+				- Remove the diff file if data is successfully uploaded 
+	***/
 	try {
 		let diffFiles = fs.readdirSync(DIFFS_REPO);
 		// Pick only yml files
@@ -151,42 +196,47 @@ export async function handleBuffer() {
 				connection.on('message', function(message) {
 					if (message.type === 'utf8') {
 						const resp = JSON.parse(message.utf8Data || "{}");
-						if (resp.type === 'auth' && resp.status != 200) { return; }
-						if (resp.type === 'sync' && resp.status === 200) { 
-							fs.unlinkSync(resp.diff_file_path);
+						if (resp.type === 'auth') {
+							if (resp.status === 200) { 
+								repoDiff.file_to_diff.forEach((fileToDiff) => {
+									const diffData = fileToDiff.diff;
+									const configFiles = configRepo['branches'][diffData.branch];
+									const relPath = diffData.file_relative_path;
+									const isBinary = diffData.is_binary;
+									const isDeleted = diffData.is_deleted;
+									const isRename = diffData.is_rename;
+									if (!isBinary && !isDeleted && !diffData.diff) {
+										putLogEvent(`Empty diff found in file: ${fileToDiff.file_path}`);
+										fs.unlinkSync(fileToDiff.file_path);
+									}
+									const fileId = configFiles[relPath];
+									// Diff data to be sent to server
+									const diffToSend = {
+										'file_id': fileId,
+										'diff': diffData.diff,
+										'is_deleted': isDeleted,
+										'is_rename': isRename,
+										'is_binary': isBinary,
+										'created_at': diffData.created_at,
+										'path': relPath,
+										'diff_file_path': fileToDiff.file_path
+									};
+									connection.send(JSON.stringify({'diffs': [diffToSend]}));	
+								});
+							} else {
+								console.log("Auth Failed");
+								return; 
+							}
+						}
+						if (resp.type === 'sync') {
+							if (resp.status === 200) { 
+								fs.unlinkSync(resp.diff_file_path);
+							}
 						}
 					}
 				});
-
-				repoDiff.file_to_diff.forEach((fileToDiff) => {
-					const diffData = fileToDiff.diff;
-					const configFiles = configRepo['branches'][diffData.branch];
-					const relPath = diffData.file_relative_path;
-					const isBinary = diffData.is_binary;
-					const isDeleted = diffData.is_deleted;
-					const isRename = diffData.is_rename;
-					if (!isBinary && !isDeleted && !diffData.diff) {
-						putLogEvent(`Empty diff found in file: ${fileToDiff.file_path}`);
-						fs.unlinkSync(fileToDiff.file_path);
-					}
-					const fileId = configFiles[relPath];
-					// Diff data to be sent to server
-					const diffToSend = {
-						'file_id': fileId,
-						'diff': diffData.diff,
-						'is_deleted': isDeleted,
-						'is_rename': isRename,
-						'is_binary': isBinary,
-						'created_at': diffData.created_at,
-						'path': relPath,
-						'diff_file_path': fileToDiff.file_path
-					};
-					connection.send(JSON.stringify({'diffs': [diffToSend]}));	
-				});
 			});
 		});
-
-		console.log("Iteration completed");
 		
 	} catch {
 		console.log("Daemon failed");
