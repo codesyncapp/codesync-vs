8f222ee7b4479da32ac4c840e66b6ff52d8ca482
Basit-qc <basit.qc@gmail.com>
2021-08-30 18:00:06 +0500

    Used jest.clearAllMocks() and separated test cases

--- tests/test_utils/utils/setup_utils.test.js
+++ tests/test_utils/utils/setup_utils.test.js
@@ -32,27 +32,26 @@ describe("createSystemDirectories",  () => {
 
 describe("setupCodeSync",  () => {
     const baseRepoPath = randomBaseRepoPath();
+    const repoPath = randomRepoPath();
     const userFilePath = `${baseRepoPath}/user.yml`;
     const userData = {"dummy_email": {access_token: "ABC"}};
 
     const configPath = `${baseRepoPath}/config.yml`;
-
-    const repoPath = randomRepoPath();
     const configData = {repos: {}};
-    configData.repos[repoPath] =  {branches: {}};
+    configData.repos[repoPath] = {branches: {}};
 
     beforeEach(() => {
-        jest.resetModules(); // Most important - it clears the cache
-        fs.mkdirSync(baseRepoPath, { recursive: true });
-        fs.mkdirSync(repoPath, { recursive: true });
+        jest.clearAllMocks();
+        fs.mkdirSync(baseRepoPath, {recursive: true});
+        fs.mkdirSync(repoPath, {recursive: true});
     });
 
     afterEach(() => {
-        fs.rmdirSync(baseRepoPath, { recursive: true });
-        fs.rmdirSync(repoPath, { recursive: true });
+        fs.rmdirSync(baseRepoPath, {recursive: true});
+        fs.rmdirSync(repoPath, {recursive: true});
     });
 
-    test('with no user.yml',  async () => {
+    test('with no user.yml', async () => {
         const port = await setupCodeSync(repoPath, baseRepoPath);
         const lsResult = fs.readdirSync(baseRepoPath);
         expect(lsResult.includes(".diffs")).toBe(true);
@@ -69,27 +68,27 @@ describe("setupCodeSync",  () => {
         expect(vscode.window.showInformationMessage.mock.calls[0][2]).toBe(NOTIFICATION.IGNORE);
     });
 
-    test('with empty user.yml',  async () => {
+    test('with empty user.yml', async () => {
         fs.writeFileSync(userFilePath, yaml.safeDump({}));
         const port = await setupCodeSync(repoPath, baseRepoPath);
         // should return port number
         expect(port).toBeTruthy();
-        expect(vscode.window.showInformationMessage).toHaveBeenCalledTimes(2);
-        expect(vscode.window.showInformationMessage.mock.calls[1][0]).toBe(NOTIFICATION.WELCOME_MSG);
-        expect(vscode.window.showInformationMessage.mock.calls[1][1]).toBe(NOTIFICATION.JOIN);
-        expect(vscode.window.showInformationMessage.mock.calls[1][2]).toBe(NOTIFICATION.IGNORE);
+        expect(vscode.window.showInformationMessage).toHaveBeenCalledTimes(1);
+        expect(vscode.window.showInformationMessage.mock.calls[0][0]).toBe(NOTIFICATION.WELCOME_MSG);
+        expect(vscode.window.showInformationMessage.mock.calls[0][1]).toBe(NOTIFICATION.JOIN);
+        expect(vscode.window.showInformationMessage.mock.calls[0][2]).toBe(NOTIFICATION.IGNORE);
         fs.rmSync(userFilePath);
     });
 
-    test('with user and repo not synced',  async () => {
+    test('with user and repo not synced', async () => {
         fs.writeFileSync(userFilePath, yaml.safeDump(userData));
         const port = await setupCodeSync(repoPath, baseRepoPath);
         // should return port number
         expect(port).toBeTruthy();
-        expect(vscode.window.showInformationMessage).toHaveBeenCalledTimes(3);
-        expect(vscode.window.showInformationMessage.mock.calls[2][0]).toBe(NOTIFICATION.CONNECT_REPO);
-        expect(vscode.window.showInformationMessage.mock.calls[2][1]).toBe(NOTIFICATION.CONNECT);
-        expect(vscode.window.showInformationMessage.mock.calls[2][2]).toBe(NOTIFICATION.IGNORE);
+        expect(vscode.window.showInformationMessage).toHaveBeenCalledTimes(1);
+        expect(vscode.window.showInformationMessage.mock.calls[0][0]).toBe(NOTIFICATION.CONNECT_REPO);
+        expect(vscode.window.showInformationMessage.mock.calls[0][1]).toBe(NOTIFICATION.CONNECT);
+        expect(vscode.window.showInformationMessage.mock.calls[0][2]).toBe(NOTIFICATION.IGNORE);
         fs.rmSync(userFilePath);
     });
 
@@ -99,10 +98,10 @@ describe("setupCodeSync",  () => {
         const port = await setupCodeSync(repoPath, baseRepoPath);
         // should return port number
         expect(port).toBeFalsy();
-        expect(vscode.window.showInformationMessage).toHaveBeenCalledTimes(4);
-        expect(vscode.window.showInformationMessage.mock.calls[3][0]).toBe(NOTIFICATION.REPO_IN_SYNC);
-        expect(vscode.window.showInformationMessage.mock.calls[3][1]).toBe(NOTIFICATION.TRACK_IT);
-        expect(vscode.window.showInformationMessage.mock.calls[3][2]).toBe(NOTIFICATION.UNSYNC_REPO);
+        expect(vscode.window.showInformationMessage).toHaveBeenCalledTimes(1);
+        expect(vscode.window.showInformationMessage.mock.calls[0][0]).toBe(NOTIFICATION.REPO_IN_SYNC);
+        expect(vscode.window.showInformationMessage.mock.calls[0][1]).toBe(NOTIFICATION.TRACK_IT);
+        expect(vscode.window.showInformationMessage.mock.calls[0][2]).toBe(NOTIFICATION.UNSYNC_REPO);
         fs.rmSync(userFilePath);
     });
 
