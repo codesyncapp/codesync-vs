ebccc56ae8ea68362d8538708bf21dbaad443b00
Basit-qc <basit.qc@gmail.com>
2022-09-21 22:42:01 +0500

    Reverted settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,33 +1,33 @@
 import path from "path";
 import untildify from "untildify";
 
-// const ROOT_REPO_NAME = '~/.codesync';
-// export const CODESYNC_WEBSOCKET_HOST = "wss://api.codesync.com";
-// export const CODESYNC_HOST = "https://api.codesync.com";
-// export const WEB_APP_URL = "https://www.codesync.com";
-// // AWS constants
-// export const LOGS_METADATA = {
-//     AWS_REGION: 'us-east-1',
-//     GROUP: 'client-logs',
-// };
-// export const PLUGIN_USER = {
-//     logStream: "codesync-common-logs",
-//     url: "https://codesync-public.s3.amazonaws.com/plugin-user.json",
-// };
-
-// TODO: Figure out better way to use dev values
-const ROOT_REPO_NAME = '~/.codesync-local-2';
-export const CODESYNC_WEBSOCKET_HOST = "ws://127.0.0.1:8000";
-export const CODESYNC_HOST = 'http://127.0.0.1:8000';
-export const WEB_APP_URL = "http://localhost:3000";
+const ROOT_REPO_NAME = '~/.codesync';
+export const CODESYNC_WEBSOCKET_HOST = "wss://api.codesync.com";
+export const CODESYNC_HOST = "https://api.codesync.com";
+export const WEB_APP_URL = "https://www.codesync.com";
+// AWS constants
 export const LOGS_METADATA = {
     AWS_REGION: 'us-east-1',
-    GROUP: 'client-logs-local'
+    GROUP: 'client-logs',
 };
 export const PLUGIN_USER = {
-    logStream: "codesync-dev-common-logs",
-    url: "https://codesync-public.s3.amazonaws.com/plugin-dev-user.json"
+    logStream: "codesync-common-logs",
+    url: "https://codesync-public.s3.amazonaws.com/plugin-user.json",
 };
+
+// TODO: Figure out better way to use dev values
+// const ROOT_REPO_NAME = '~/.codesync-local-2';
+// export const CODESYNC_WEBSOCKET_HOST = "ws://127.0.0.1:8000";
+// export const CODESYNC_HOST = 'http://127.0.0.1:8000';
+// export const WEB_APP_URL = "http://localhost:3000";
+// export const LOGS_METADATA = {
+//     AWS_REGION: 'us-east-1',
+//     GROUP: 'client-logs-local'
+// };
+// export const PLUGIN_USER = {
+//     logStream: "codesync-dev-common-logs",
+//     url: "https://codesync-public.s3.amazonaws.com/plugin-dev-user.json"
+// };
 
 export const generateSettings = () => {
     const rootRepo = untildify(ROOT_REPO_NAME);
