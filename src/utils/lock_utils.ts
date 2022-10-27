380c833cd1f744a91798b5add904d56cfb939eae
Basit-qc <basit.qc@gmail.com>
2022-10-27 15:26:39 +0500

    Using onCompromised for tests

--- src/utils/lock_utils.ts
+++ src/utils/lock_utils.ts
@@ -1,4 +1,4 @@
-import lockFile from 'proper-lockfile';
+import lockFile, { LockOptions } from 'proper-lockfile';
 import { generateSettings } from '../settings';
 import { CodeSyncState, CODESYNC_STATES } from './state_utils';
 
@@ -9,9 +9,11 @@ const onCompromised = () => {
 export class LockUtils {
 	
 	settings: any;
+	lockOptions: LockOptions;
 
 	constructor() {
         this.settings = generateSettings();
+		this.lockOptions = ((global as any).IS_CODESYNC_DEV) ? {onCompromised: onCompromised}: {};
 	}
 
 	checkPopulateBufferLock () {
@@ -32,7 +34,7 @@ export class LockUtils {
 	
 	acquirePopulateBufferLock = () => {
 		try {
-			lockFile.lockSync(this.settings.POPULATE_BUFFER_LOCK_FILE, {onCompromised: onCompromised});
+			lockFile.lockSync(this.settings.POPULATE_BUFFER_LOCK_FILE, this.lockOptions);
 			CodeSyncState.set(CODESYNC_STATES.POPULATE_BUFFER_LOCK_ACQUIRED, true);
 		} catch (e) {
 			// 
@@ -41,7 +43,7 @@ export class LockUtils {
 	
 	acquireSendDiffsLock = () => {	
 		try {
-			lockFile.lockSync(this.settings.DIFFS_SEND_LOCK_FILE, {onCompromised: onCompromised});
+			lockFile.lockSync(this.settings.DIFFS_SEND_LOCK_FILE, this.lockOptions);
 			CodeSyncState.set(CODESYNC_STATES.DIFFS_SEND_LOCK_ACQUIRED, true);
 		} catch (e) {
 			// 
@@ -50,7 +52,7 @@ export class LockUtils {
 
 	acquirePricingAlertLock () {
 		try {
-			lockFile.lockSync(this.settings.UPGRADE_PLAN_ALERT, {onCompromised: onCompromised});
+			lockFile.lockSync(this.settings.UPGRADE_PLAN_ALERT, this.lockOptions);
 		} catch (e) {
 			// 
 		}
