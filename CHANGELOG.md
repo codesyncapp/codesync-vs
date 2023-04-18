3fcf31b0aeb583585bcf0bb7324b3227238ec224
Basit-qc <basit.qc@gmail.com>
2023-04-18 10:31:03 +0500

    Few more test cases added for missing branch-statements

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [3.19.0] - 2023-04-17
+## [3.19.0] - 2023-04-18
 - Optimized logic to checkForRename using [string-similarity](https://www.npmjs.com/package/string-similarity)
     - Earlier logic was taking ~15s for comparing text of files of ~50KB. With "string-similarity" it is in miliseconds
 - Fixed a bug in getting `PotentialMatchingFiles` for rename
--- tests/test_utils/common/getSkipRepos.test.js
+++ tests/test_utils/common/getSkipRepos.test.js
@@ -8,14 +8,14 @@ import { IGNORABLE_DIRECTORIES } from "../../../src/constants";
 describe("getSkipPaths",  () => {
     const repoPath = randomRepoPath();
     const syncIgnorePath = getSyncIgnoreFilePath(repoPath);
-    const syncIgnoreData = ".skip_repo_1\n\n\n.skip_repo_2\n!.skip_repo_3";
+    const syncIgnoreData = ".skip_repo_1/\n\n\nskip_repo_2/**\n!.skip_repo_3/*\n.skip_repo_4\file.js";
     const defaultSkipPaths = [...IGNORABLE_DIRECTORIES.map(ignoreDir => `${repoPath}/**/${ignoreDir}/**`)];
 
     beforeEach(() => {
         // Create directories
         fs.mkdirSync(repoPath, { recursive: true });
         fs.mkdirSync(path.join(repoPath, ".skip_repo_1"), { recursive: true });
-        fs.mkdirSync(path.join(repoPath, ".skip_repo_2"), { recursive: true });
+        fs.mkdirSync(path.join(repoPath, "skip_repo_2"), { recursive: true });
         fs.writeFileSync(path.join(repoPath, "file.js"), "");
     });
 
@@ -23,17 +23,18 @@ describe("getSkipPaths",  () => {
         fs.rmSync(repoPath, { recursive: true, force: true });
     });
 
+    test('skipRepos with no .syncignore, default values', () => {
+        const skippedRepos = getSkipPaths(repoPath, []);
+        expect(skippedRepos).toStrictEqual(defaultSkipPaths);
+    });
+
     test('skipRepos with .syncignore items', () => {
         fs.writeFileSync(syncIgnorePath, syncIgnoreData);
         let syncIgnoreItems = getSyncIgnoreItems(repoPath);
         const skippedRepos = getSkipPaths(repoPath, syncIgnoreItems);
-        const syncIgnoreItemsForGlob = syncIgnoreItems.map(item => `${repoPath}/${item}/**`);
-        expect(skippedRepos).toEqual([...defaultSkipPaths, ...syncIgnoreItemsForGlob]);
-    });
-
-    test('skipRepos with NO .syncignore, default values', () => {
-        const skippedRepos = getSkipPaths(repoPath, []);
-        expect(skippedRepos).toStrictEqual(defaultSkipPaths);
+        // skip_repo_4 is non existant
+        expect(skippedRepos.filter(x => !defaultSkipPaths.includes(x))).toEqual([".skip_repo_1/**", "skip_repo_2/**"].map(item => `${repoPath}/${item}`));
+        expect(skippedRepos.filter(x => !defaultSkipPaths.includes(x)).includes(".skip_repo_4")).toEqual(false);
     });
 
     test('with non-existing .syncignore item', () => {
@@ -43,7 +44,7 @@ describe("getSkipPaths",  () => {
 
     test('with file in syncignore', () => {
         fs.writeFileSync(syncIgnorePath, syncIgnoreData);
-        const skippedRepos = getSkipPaths(repoPath, ["file.js"]);
+        const skippedRepos = getSkipPaths(repoPath, ['file.js']);
         expect(skippedRepos).toStrictEqual(defaultSkipPaths);
     });
 });
