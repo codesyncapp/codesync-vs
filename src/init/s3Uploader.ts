30b2211872431fcac949a46415ee4fa910f8e8b9
Basit-qc <basit.qc@gmail.com>
2023-08-08 02:20:21 +0500

    WIP for processing Tasks

--- src/init/s3Uploader.ts
+++ src/init/s3Uploader.ts
@@ -1,27 +1,21 @@
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
-<<<<<<< HEAD
 import { glob } from 'glob';
 import isOnline from 'is-online';
 import parallel from "run-parallel";
-=======
->>>>>>> b71df49 (WIP: Saving URLs and processing is WIP)
 
 import { readYML } from '../utils/common';
 
 import { generateSettings } from "../settings";
 import { uuidv4 } from '../utils/setup_utils';
 import { removeFile } from '../utils/file_utils';
-<<<<<<< HEAD
 import { pathUtils } from '../utils/path_utils';
 import { uploadFileTos3_ } from '../utils/upload_utils';
 import { CodeSyncLogger } from '../logger';
 import { CODESYNC_STATES, CodeSyncState } from '../utils/state_utils';
 import { IS3UploaderFile, IS3UploaderPreProcess } from '../interface';
 import { S3_UPLOADR_RETRY_AFTER, S3_UPLOAD_TIMEOUT } from '../constants';
-=======
->>>>>>> b71df49 (WIP: Saving URLs and processing is WIP)
 
 
 export class s3Uploader {
@@ -32,18 +26,13 @@ export class s3Uploader {
 	We'll have YML files in .s3_uploader.
 	Each file will look like
 
-<<<<<<< HEAD
 		repo_path: /Users/dev/codesync/codesync-vs
-=======
-		repo_id: 1234
->>>>>>> b71df49 (WIP: Saving URLs and processing is WIP)
 		branch: master
 		file_path_and_urls:
 			file_path_1: url_1
 			file_path_2: url_2
 			...
 		locked_by: uuid
-<<<<<<< HEAD
 		failed_count: 0
 	
 	Flow:
@@ -270,55 +259,3 @@ export class s3Uploader {
 		}
 	}
 }
-=======
-	
-	*/
-
-	settings: any;
-	uuid: string;
-	config: any;
-
-	constructor() {
-		this.settings = generateSettings();
-		this.config = readYML(this.settings.CONFIG_PATH);
-		this.uuid = uuidv4();
-	}
-
-	saveURLs (repoId: string, branch: string, filePathsAndURLs: any) {
-		/*
-			Once we receive the response of /v1/init from server, we save presigned URLs inside .s3_uploader/
-		*/
-		const data = {
-			repo_id: repoId, 
-			branch: branch,
-			file_path_and_urls: filePathsAndURLs,
-			locked_by: this.uuid
-		};
-
-		const fileName = `${new Date().getTime()}.yml`;
-		const filePath = path.join(this.settings.S3_UPLOADER, fileName);
-		fs.writeFileSync(filePath, yaml.dump(data));
-		return filePath;
-	}
-
-	async process (filePath: string) {
-		if (!fs.existsSync(filePath)) return;
-		const content = readYML(filePath);
-		// if some other instance of s3Uploader is processing this file, skip it
-		if (content.locked_by && content.locked_by !== this.uuid) return;
-		let repoConfig = null;
-		// Process file
-		for (const key of this.config.repos) {
-			repoConfig = this.config.repos[key];
-			repoConfig = repoConfig.id === content.repo_id && !repoConfig.is_disconnected ? repoConfig : null;
-		}
-		// Remove file if repoConfig is not found
-		if (!repoConfig) return removeFile(filePath, "s3Uploader.process");
-
-	}
-
-	static run () {
-		// 
-	}
-}
->>>>>>> b71df49 (WIP: Saving URLs and processing is WIP)
