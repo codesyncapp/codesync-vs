96d7e80f9522ff81c4c33d2688b9bcce298c7d6e
Hafiz Muhammad Basit <basit.qc@gmail.com>
2023-12-14 18:51:55 +0500

    Fix for Window path for gitExtension

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [3.34.1] - 2023-12-14
+- Fix for Window path
+
 ## [3.34.0] - 2023-12-14
 - Sending commit_hashes along with diffs to the server
 
--- src/utils/setup_utils.ts
+++ src/utils/setup_utils.ts
@@ -260,7 +260,7 @@ export const registerGitListener = async (repoPath: string) => {
 	// Check if the workspace is a Git repository, use the Git extension API to get Git information
 	const gitExtension = await getBuiltInGitApi();
 	if (!gitExtension) return;
-	const gitRepository = gitExtension.repositories.find(repo => repo.rootUri.fsPath === repoPath);
+	const gitRepository = gitExtension.repositories.find(repo => pathUtils.normalizePath(repo.rootUri.fsPath) === repoPath);
 	if (!gitRepository) {
 		console.log("Is not a gitRepo");
 		return;
@@ -270,7 +270,7 @@ export const registerGitListener = async (repoPath: string) => {
 	CodeSyncState.set(CODESYNC_STATES.GIT_COMMIT_HASH, currentCommit);
 	CodeSyncLogger.debug(`CommitHash=${currentCommit}`);
 	gitRepository.state.onDidChange(() => {
-		const gitRepository = gitExtension.repositories.find(repo => repo.rootUri.fsPath === repoPath);
+		const gitRepository = gitExtension.repositories.find(repo => pathUtils.normalizePath(repo.rootUri.fsPath) === repoPath);
 		if (!gitRepository) return;	
 		const newCommitHash = gitRepository.state.HEAD?.commit;
 		if (!newCommitHash) return;
