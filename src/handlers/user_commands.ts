399828190dec172cfb167fa0730e497c59e5eddd
Zahid Saeed <muhammadzahid1099@gmail.com>
2024-08-13 18:02:40 +0500

    update notification message for requesting user to submit form

--- src/handlers/user_commands.ts
+++ src/handlers/user_commands.ts
@@ -14,16 +14,7 @@ export const authHandler = (skipAskConnect=false) => {
 export const requestDemoUrl = () => {
 	const authUrl = generateRequestDemoUrl();
 	vscode.env.openExternal(vscode.Uri.parse(authUrl));
-
-	vscode.window.showInformationMessage(
-		NOTIFICATION.REQUEST_MSG_FOR_DEMO, ...[
-		NOTIFICATION.REQUEST_DEMO,
-		NOTIFICATION.IGNORE
-	]).then(async selection => {
-		if (selection === NOTIFICATION.REQUEST_DEMO) {
-			requestDemoUrl();
-		}
-	});
+	vscode.window.showInformationMessage(NOTIFICATION.REQUEST_MSG_FOR_DEMO);
 };
 
 export const logoutHandler = () => {
